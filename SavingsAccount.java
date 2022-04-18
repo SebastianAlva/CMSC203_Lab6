e99c80d92aceaf22e55aa8a2e5dffcce4e709a5c
Sebastian Alva <98499668+SebastianAlva@users.noreply.github.com>
2022-04-18 13:51:55 -0400

    Add files via upload

---
+++ SavingsAccount.java
@@ -0,0 +1,42 @@
+public class SavingsAccount extends BankAccount 
+{
+	private double rate = 0.025;
+	private int identifier = 0;
+	private String accountNumber;
+	
+	public SavingsAccount(String name, double balance) 
+	{
+		super(name, balance);
+		
+		// Set account number
+		this.accountNumber = super.getAccountNumber() + "-" + identifier;
+	}
+	
+	public SavingsAccount(SavingsAccount account, double balance)
+	{
+		// Call parent constructor
+		super(account, balance);
+		
+		// Set new account number
+		identifier++;
+		accountNumber = super.getAccountNumber() + "-" + identifier;
+	}
+
+	/**
+	 * Post interest method
+	 * 
+	 * Deposits the appropriate amount of interest into the account
+	 */
+	public void postInterest() {
+		// Calculate interest
+		double balance = getBalance();
+		double interest = balance * (1 + (rate / 12));
+		
+		// Set new balance
+		setBalance(interest);
+	}
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+}
